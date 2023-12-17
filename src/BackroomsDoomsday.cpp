5ca3b4af9cd8cb697ef5f0d134e166b7237961a4
Donato-Curvino <donato@tamu.edu>
2023-12-17 15:36:00 -0600

    added Clock, Map, and initialized a VertexArray for rendering

--- src/BackroomsDoomsday.cpp
+++ src/BackroomsDoomsday.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    window.setFramerateLimit(60);
+#include "engine/Map.h"
 
+#define WIDTH 800
+#define HEIGHT 600
+
+using std::string;
+
+int main() {
+    // INITIALIZE GAME OBJECTS
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My window");
+    window.setFramerateLimit(60);
+    sf::Clock clock;
+    sf::VertexArray walls(sf::Lines, WIDTH * 2);
+    
+    Map map("backroom.png");
+
+    // GAME LOOP
     while (window.isOpen()) {
+        sf::Time dt = clock.restart();
+        
+        // EVENT LOOP
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
         
-        window.clear(sf::Color(0, 255, 0, 255));
 
+        window.clear(sf::Color(0, 255, 0, 255));
+        // TODO: draw screen
+        // window.draw(map);
         window.display();
     }
     return 0;
