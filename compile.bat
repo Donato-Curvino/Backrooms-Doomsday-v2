35ad8da5c91efe992e33b2e73c939d5c1bf6b7fd
Donato-Curvino <donato@tamu.edu>
2023-12-17 15:37:17 -0600

    edited compile script

--- compile.bat
+++ compile.bat
@@ -1,2 +1,15 @@
-g++ -o bin/BackroomsDoomsdayTST.exe -static-libgcc -static-libstdc++ -gdwarf-2 src/BackroomsDoomsday.cpp -lsfml-window -lsfml-system -lsfml-graphics
-bin\BackroomsDoomsdayTST.exe
+@echo off
+
+windres src/addIcon.rc -O coff -o bin/build/my.res
+g++ -o bin/BackroomsDoomsdayTST.exe -static-libgcc -static-libstdc++ -gdwarf-2 ^
+    src/BackroomsDoomsday.cpp src/engine/Map.cpp bin/build/my.res ^
+    -lsfml-window -lsfml-system -lsfml-graphics
+
+@REM g++ -o bin/BackroomsDoomsdayTST.exe -mwindows -static-libgcc -static-libstdc++ -gdwarf-2 src/BackroomsDoomsday.cpp bin/build/my.res -lsfml-window -lsfml-system -lsfml-graphics
+
+IF %ERRORLEVEL% NEQ 0 Goto :Done
+cd bin
+BackroomsDoomsdayTST.exe
+cd ..
+
+:Done
