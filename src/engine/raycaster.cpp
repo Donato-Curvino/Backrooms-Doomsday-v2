4c2347a8781e32d727c929284460e4be7fcd74eb
Donato-Curvino <donato@tamu.edu>
2023-12-19 00:25:14 -0600

    raycasting almost there, just slightly off

--- src/engine/raycaster.cpp
+++ src/engine/raycaster.cpp
@@ -37,7 +37,7 @@ float raycast(const Map* map, const sf::Vector2f* pos, float angle, sf::Vertex* 
     // std::cout << std::endl;
 
     // check vertical
-    dy = v_step / std::tan(angle); 
+    dx = v_step / std::tan(angle); 
     while (!map->collide(v_endx, v_endy)) {
         v_endx += dx;       v_endy += v_step;
     }
@@ -51,17 +51,17 @@ float raycast(const Map* map, const sf::Vector2f* pos, float angle, sf::Vertex* 
         dist = h_dist;
     }
 
-    // if (line != nullptr) {
-    //     line[0] = sf::Vertex(*pos, sf::Color::Red);
-    //     line[1] = sf::Vertex(sf::Vector2f(h_endx, h_endy), sf::Color::Black);
-    //     line[2] = sf::Vertex(*pos, sf::Color::Yellow);
-    //     line[3] = sf::Vertex(sf::Vector2f(v_endx, v_endy), sf::Color::Yellow);
-    // }
-
     if (line != nullptr) {
-        line[0] = sf::Vertex(*pos, sf::Color::Magenta);
-        line[1] = sf::Vertex(h_is_min ? sf::Vector2f(h_endx, h_endy) : sf::Vector2f(v_endx, v_endy), sf::Color::Magenta);
+        line[0] = sf::Vertex(*pos, sf::Color::Red);
+        line[1] = sf::Vertex(sf::Vector2f(h_endx, h_endy), sf::Color::Red);
+        line[2] = sf::Vertex(*pos, sf::Color::Yellow);
+        line[3] = sf::Vertex(sf::Vector2f(v_endx, v_endy), sf::Color::Yellow);
     }
+
+    // if (line != nullptr) {
+    //     line[0] = sf::Vertex(*pos, sf::Color::Magenta);
+    //     line[1] = sf::Vertex(h_is_min ? sf::Vector2f(h_endx, h_endy) : sf::Vector2f(v_endx, v_endy), sf::Color::Magenta);
+    // }
 
     // std::cout << dist << std::endl;
     return dist;
