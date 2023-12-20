e9d4af363b0a68178f637c88b7302735dce7f497
Donato-Curvino <donato@tamu.edu>
2023-12-20 15:13:37 -0600

    modified compilation scripts

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ ifeq ($(OS), Windows_NT)
 	DEBUG_FLAGS=-static-libgcc -static-libstdc++ -gdwarf-2
 	FILE_EXTENSION=.exe
 else
-	DEBUG_FLAGS=-g
+	DEBUG_FLAGS=-g -fsanitize=address,undefined
 	FILE_EXTENSION=.out
 endif
 
--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @echo off
 
 windres src/addIcon.rc -O coff -o bin/build/my.res
-g++ -o bin/BackroomsDoomsdayTST.exe -static-libgcc -static-libstdc++ -gdwarf-2 ^
+g++ -o bin/BackroomsDoomsdayTST.exe -std=c++17 -static-libgcc -static-libstdc++ -gdwarf-2 ^
     src/BackroomsDoomsday.cpp src/engine/*.cpp src/components/*.cpp bin/build/my.res ^
     -lsfml-window -lsfml-system -lsfml-graphics
 
